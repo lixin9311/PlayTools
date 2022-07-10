e62b6c317709a3d015feb26a9d2cce9af1e3b575
lucas lee <lixin9311@gmail.com>
2022-07-10 14:06:51 +0900

    remove package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LibPlayTools",
+            name: "PlayTools",
             targets: ["PlayToolsCore", "PlayLoader", "PTFakeTouch"]),
     ],
     dependencies: [
