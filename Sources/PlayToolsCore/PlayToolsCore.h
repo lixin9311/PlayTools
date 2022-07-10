8bd18b38b61fbb03ca7a0bcc692b7430a9a91412
lucas lee <lixin9311@gmail.com>
2022-07-10 14:05:17 +0900

    test

---
+++ Sources/PlayToolsCore/PlayToolsCore.h
@@ -0,0 +1,19 @@
+//
+//  PlayTools.h
+//  PlayTools
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayTools.
+FOUNDATION_EXPORT double PlayToolsVersionNumber;
+
+//! Project version string for PlayTools.
+FOUNDATION_EXPORT const unsigned char PlayToolsVersionString[];
+
+#import "PTFakeMetaTouch.h"
+#import "IOHIDEvent+KIF.h"
+#import "UIApplication+Private.h"
+#import "UIEvent+Private.h"
+#import "UITouch+Private.h"
